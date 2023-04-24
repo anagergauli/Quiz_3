651c82e054bfb1aeff628cff4e3002a14c1d7ea1
anagergauli <128690850+anagergauli@users.noreply.github.com>
2023-04-25 00:58:41 +0400

    Add files via upload

---
+++ Task_2_CustomQueueGenericImplementation/Program.cs
@@ -0,0 +1,49 @@
+﻿//Task_2, Quiz_3
+
+//custom queue implementation
+//adding and removing elements
+//with and without parametrized constructors
+
+using System;
+using System.Collections.Generic;
+
+public class CustomQueue<T>
+{
+    private List<T> elements;
+
+    public CustomQueue()
+    {
+        elements = new List<T>();
+    }
+
+    public CustomQueue(int capacity)
+    {
+        elements = new List<T>(capacity);
+    }
+
+    public int Count => elements.Count;
+
+    public bool IsEmpty => Count == 0;
+
+    public void Enqueue(T element) //adding elements
+    {
+        elements.Add(element);
+    }
+
+    public T Dequeue() //removing elements
+    {
+        if (IsEmpty)
+        {
+            throw new InvalidOperationException("Queue is empty.");
+        }
+
+        T element = elements[0];
+        elements.RemoveAt(0);
+        return element;
+    }
+
+    public void Clear() //removing all elements
+    {
+        elements.Clear();
+    }
+}
